d4eb140a85e0effa592ccc24bdb2c9fc73b10167
Murthy-sys <murthymalisetti@gmail.com>
2026-07-19 22:01:08 +0530

    fixes ap per request

--- src/constants/siteConstants.js
+++ src/constants/siteConstants.js
@@ -1,6 +1,6 @@
 export const whatsappUrl = "https://wa.me/919000382815?text=Namaste%2C%20I%27d%20like%20to%20know%20more%20about%20classes%20at%20Suresh%27s%20Yogalaya.";
 export const gayatriWhatsappUrl = "https://wa.me/919686570032?text=Namaste%2C%20I%27d%20like%20to%20know%20more%20about%20classes%20at%20Suresh%27s%20Yogalaya.";
-export const justdialReviewsUrl = "https://share.google/iUKOW55UqZQT2GuQ4";
+export const justdialReviewsUrl = "https://share.google/rAYDu38gDtA5twySM";
 
 export const navigationLinks = [
   ["Instructors", "instructor"],
